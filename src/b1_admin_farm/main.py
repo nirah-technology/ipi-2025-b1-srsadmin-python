a6774e84607839faadea291d579da6a6c6843e99
Nicolas Metivier <nicolas.a.metivier@gmail.com>
2025-11-25 11:15:13 +0100

    Add imports

--- src/b1_admin_farm/main.py
+++ src/b1_admin_farm/main.py
@@ -1,18 +1,12 @@
-def create_film():
-    titre = input("Entrez le titre du film : ")
-    description = input("Entrez la description du film : ")
-    note = input("Entrez la note du film : ")
-    return {"titre": titre, "description": description, "note": note}
+from .evolve import Cat
+from .movies import Film
+
 
 def main():
-    bibliotheque = []
-    film = create_film()
-    bibliotheque.append(film)
+    cat = Cat("Nitro")
 
 if __name__ == "__main__":
     main()
-
-
 
 
 
